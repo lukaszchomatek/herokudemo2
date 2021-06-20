a9486290a579e6d2a9c139f8f6923cc0422ee223
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2021-06-20 13:48:05 +0200

    base app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World from IPI classes!')
+})
+
+app.get('/users', (req, res) => {
+    let users = [
+        {
+            name: "Jan",
+            surname: "Kowalski"
+        },
+        {
+            name: "Anna",
+            surname: "Kowalska"
+        }
+    ]
+    res.send(users)
+  })
+///Bardzo istotne jest app.listen w takiej formie!!!
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
